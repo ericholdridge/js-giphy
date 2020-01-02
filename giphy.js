eb9d30ffc94c4e70b3cb2bd941b8ec8e9324b165
ericholdridge <eholdridge2@gmail.com>
2020-01-01 19:00:40 -0600

    Implement search feature

--- giphy.js
+++ giphy.js
@@ -1,14 +1,36 @@
-const api = 'https://api.giphy.com/v1/gifs/trending?api_key=4hLxRWpsWJ7ksJHmkTiTfuhGXqWWA6BQ&limit=25&rating=G';
+const apiKey = '4hLxRWpsWJ7ksJHmkTiTfuhGXqWWA6BQ';
 
-// Fetch the api
-fetch(api)
-    // Get the response and return it as JSON
-    .then(reponse => {
-        return reponse.json();
-    })
-    // Take the data and display it in the console
-    .then(data => {
-        console.log(data);
-    })
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+    document.getElementById('btnSearch').addEventListener('click', event => {
+        event.preventDefault();
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=`;
+        let str = document.getElementById('search').value.trim();
+        url = url.concat(str);
+        console.log(url);
+
+        // Fetch the api
+        fetch(url)
+            // Get the response and return it as JSON
+            .then(reponse => {
+                return reponse.json();
+            })
+            // Take the data and display it in the console
+            .then(data => {
+                console.log(data);
+                let fig = document.createElement("figure");
+                let img = document.createElement("img");
+                let fc = document.createElement("figcaption");
+                img.src = data.data[0].images.downsized.url;
+                img.alt = data.data[0].title;
+                fc.textContent = data.data[0].title;
+                fig.appendChild(img)
+                fig.appendChild(fc);
+                let out = document.querySelector('.out');
+                out.insertAdjacentElement("afterbegin", fig);
+            });
+    });
+}
+
 
     
